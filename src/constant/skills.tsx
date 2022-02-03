9672df387ddce2ff2e9edf9a9fd864f27b21fc09
JasonK369 <jason10101996@gmail.com>
2022-02-04 00:58:38 +0800

    feat: add skill set table

---
+++ src/constant/skills.tsx
@@ -0,0 +1,11 @@
+const skills = [
+    {skill: "Java", rating: 70},
+    {skill: "Spring Boot", rating: 70},
+    {skill: "Ruby", rating: 60},
+    {skill: "Ruby On Rails", rating: 60},
+    {skill: "JavaScript", rating: 50},
+    {skill: "TypeScript", rating: 50},
+    {skill: "React", rating: 50},
+]
+
+export default skills;
