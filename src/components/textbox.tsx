97b8a40e0ae4b47d69b0756a3b50a926ab850e98
JasonK369 <jason10101996@gmail.com>
2022-02-03 22:29:27 +0800

    feat: create textbox

---
+++ src/components/textbox.tsx
@@ -0,0 +1,40 @@
+import * as React from "react"
+import Layout from '../components/layout'
+import styled from 'styled-components'
+
+interface IProps {
+    title: string,
+    content: string,
+    children?: JSX.Element
+};
+
+const Wrapper = styled.div`
+    background-color: #42BFDD; // title color
+`
+
+const Title = styled.h2`
+    padding: 10px 0px 0px 10px;
+    margin-bottom: 10px;
+`
+
+const ContentWrapper = styled.div`
+    background-color: #084B83; // content background color
+    padding: 10px;
+    margin-bottom: 10px;
+    color: #F0F6F6 // word color
+`
+
+const textbox = ({ title, content, children }: IProps) => {
+    return (
+        <Wrapper>
+            <Title>
+                {title}
+            </Title>
+            <ContentWrapper>
+                {children || content}
+            </ContentWrapper>
+        </Wrapper>
+    )
+}
+
+export default textbox;
