68f8ee2b8df695fdf94ab93d31533425bf34a243
JasonK369 <jason10101996@gmail.com>
2022-02-03 22:28:23 +0800

    feat: create experience constant

---
+++ src/constant/experience.tsx
@@ -0,0 +1,24 @@
+const experiences = [{
+    title: "July 2021 - Present",
+    company: "SHOPLINE",
+    position: "Software Engineer",
+    location: "Hong Kong",
+    descriptions: ['Responsible for merchant subscription', 'Frontend development with React', 'Backend development with Ruby on Rails']
+},
+{
+    title: "Oct 2020 - May 2021",
+    company: "HKTV",
+    position: "Porgrammer II",
+    location: "Hong Kong",
+    descriptions: ['Lead a small team of programmer', 'Responsible for payment service development and maintenance', 'Cooperate with 3rd party for payment gateway integration']
+},
+{
+    title: "Jul 2019 - Oct 2020",
+    company: "HKTV",
+    position: "Porgrammer I",
+    location: "Hong Kong",
+    descriptions: ['Responsible for payment service', 'Developer for HKTVpay(Backend)', 'Involved in system design for HKTVpay(Backend)', 'Mainly use Spring Boot(Java)']
+}
+]
+
+export default experiences;
