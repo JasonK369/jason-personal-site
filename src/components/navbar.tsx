5c58a1763f37f2d81615243dc06e310cc57f24f0
JasonK369 <17778562+JasonK369@users.noreply.github.com>
2022-08-31 10:41:58 +0800

    fix: remove background for navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,23 +4,23 @@ import { AnchorLink } from "gatsby-plugin-anchor-links";
 
 const Navbar = styled.nav`
   display: flex;
-  background-color: #fff;
+  flex-direction: column;
   position: relative;
   justify-content: space-between;
-  border-bottom: 2px solid #33333320;
   margin: 0 auto;
   z-index: 2;
   align-self: center;
   min-height: 50px;
-  flex-direction: row;
+  align-items: center;
+  padding-top: 5px;
 
 
   @media (max-width: 768px) {
-    background-color: #ff0000;
+    background-color: #9f9eb5;
     display: flex;
     flex-direction: column;
     position: sticky;
-    height: 8vh;
+    height: 10vh;
     top: 0;
     left: 0;
     right: 0;
@@ -29,43 +29,60 @@ const Navbar = styled.nav`
 `
 const Content = styled.div`
   display: flex;
-  // align-self: left;
   width: 50vw;
+  align-items: center;
+  justify-content: center;
+  padding: 5px;
+
+  @media (max-width: 768px) {
+    margin: 0px 1vw;
+    padding: 0px;
+  }
 `
 
 const Item = styled.div`
-  padding-left: 30px;
+  margin: 0px 4vw;
+  a:link, a:visited {
+    color: #7df5c7;
+  }
+  @media (max-width: 768px) {
+    margin: 0px 2vw;
+  }
+
+  @media (max-width: 375px) {
+    margin: 0px 1vw;
+  }
 `
 
 
 const navBar = () => {
-    return (
-        <Navbar>
-          <Content>
-            <Item>
-              <AnchorLink to="#aboutMe" title="About Me">
-                About Me
-              </AnchorLink>
-            </Item>
-            <Item>
-            <AnchorLink to="#experience" title="Past Experience">
-              Experience
-            </AnchorLink>
-            </Item>
-            <Item>
-            <AnchorLink to="#skill" title="Skills">
-              Skills
-            </AnchorLink>
-            </Item>
-          </Content>
-        </Navbar>
-        // <Link to="https://www.linkedin.com/in/jason-kwong-hk/">
-        //       <NavLinkText>
-        //                          LinkedIn
-        //                      </NavLinkText>
-        //                  </Link>
-        
-    )
+  return (
+    <Navbar>
+      <Content>
+        <Item>
+          <AnchorLink to="#aboutMe" title="About Me">
+            About Me
+          </AnchorLink>
+        </Item>
+        <Item>
+          <AnchorLink to="#experience" title="Past Experience">
+            Experience
+          </AnchorLink>
+        </Item>
+        <Item>
+          <AnchorLink to="#skill" title="Skills">
+            Skills
+          </AnchorLink>
+        </Item>
+      </Content>
+    </Navbar>
+    // <Link to="https://www.linkedin.com/in/jason-kwong-hk/">
+    //       <NavLinkText>
+    //                          LinkedIn
+    //                      </NavLinkText>
+    //                  </Link>
+
+  )
 }
 
 export default navBar;
