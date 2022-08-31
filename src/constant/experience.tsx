81ddd8091cc9fd0c7213faa1cab59b556f214315
JasonK369 <17778562+JasonK369@users.noreply.github.com>
2022-08-31 09:46:30 +0800

    fix: fix typo

--- src/constant/experience.tsx
+++ src/constant/experience.tsx
@@ -8,14 +8,14 @@ const experiences = [{
 {
     title: "Oct 2020 - May 2021",
     company: "HKTV",
-    position: "Porgrammer II",
+    position: "Programmer II",
     location: "Hong Kong",
     descriptions: ['Lead a small team of programmer', 'Responsible for payment service development and maintenance', 'Cooperate with 3rd party for payment gateway integration']
 },
 {
     title: "Jul 2019 - Oct 2020",
     company: "HKTV",
-    position: "Porgrammer I",
+    position: "Programmer I",
     location: "Hong Kong",
     descriptions: ['Responsible for payment service', 'Developer for HKTVpay(Backend)', 'Involved in system design for HKTVpay(Backend)', 'Mainly use Spring Boot(Java)']
 }
